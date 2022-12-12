85effc06447c80e9c8b72d685883ddaa7276e6d2
Cho Hyunho <chohh02@gmail.com>
2022-12-12 23:12:48 +0900

    Feat add SecondFile

--- gitTest/gitTest/SecondFile.swift
+++ gitTest/gitTest/SecondFile.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct SecondFile: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("LikeLion Group 6")
+            Text("Mini's git school")
+            Text("The big TV !")
+        }
     }
 }
 
