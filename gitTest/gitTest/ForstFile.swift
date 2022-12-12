2ba2a7d1be8159e272b7bd5c05fe95af9b2f6d78
Lianne <yenchoi010@gmail.com>
2022-12-12 23:11:15 +0900

    initial commit

---
+++ gitTest/gitTest/ForstFile.swift
@@ -0,0 +1,12 @@
+//
+//  ForstFile.swift
+//  gitTest
+//
+//  Created by yeeunchoi on 2022/12/12.
+//
+
+import Foundation
+
+let forst: String = "Jinni"
+let euniName: String = "Yeeun"
+let euniAge: Int = 20
