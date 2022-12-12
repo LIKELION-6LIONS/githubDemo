70e5c3dbaa519aeed80f7eaa0710199fe16615ec
ryuchanghwi <ckdgnl10@naver.com>
2022-12-12 23:11:11 +0900

    create changhwiView

---
+++ gitTest/gitTest/ChanghwiView.swift
@@ -0,0 +1,20 @@
+//
+//  ChanghwiView.swift
+//  gitTest
+//
+//  Created by 류창휘 on 2022/12/12.
+//
+
+import SwiftUI
+
+struct ChanghwiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChanghwiView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChanghwiView()
+    }
+}
